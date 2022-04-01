da88a7d966845baadbeb0ec834f5e750e9c5c3aa
Fabiano Moreira <fabiano.moreira@atak.com.br>
2022-04-01 20:01:45 -0300

    2022-04-01

---
+++ app/services/DAO.php
@@ -0,0 +1,20 @@
+<?php
+
+class DAO
+{
+    private $pdo;    
+
+    function.conecta(){
+        try{
+            //new PDO('mysql:host=localhost;port=8888;dbname=test', $user, $pass);
+            $pdo = new PDO(DRIVE .":host=".HOST.";port=".PORT.";dbname=".DB , USER, PASS);
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e)
+        {
+            echo("<b>Erro PDO:</b> ". $e->getMessage() ."</br>");
+            $pdo = null;
+        }
+        return $pdo;
+    }
+}
