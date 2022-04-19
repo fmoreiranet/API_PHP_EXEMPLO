dd1ccf524741e7e94c38de866b556f3fe182fe7b
Fabiano Moreira <fabiano.moreira@atak.com.br>
2022-04-18 21:02:40 -0300

    correções no projeto

--- config/config.php
+++ config/config.php
@@ -1,4 +1,5 @@
 <?php
+include_once("secrets.php");
 
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
@@ -10,7 +11,7 @@ define("DRIVE", "mysql"); // Qual tipo de banco de dados
 define("HOST", "localhost"); // url ou ip do local do banco de dados
 define("PORT", "3306"); // Porta de acesso ao banco de dados
 define("USER", "root"); //Usuario do banco de dados
-define("PASS", ""); // Senha do banco de dados
+//define("PASS", ""); // Senha do banco de dados
 define("DB", "loja-exemplo"); //Nome do banco de dados
 
-define("KEY", "ff08e69475562803be134abe13fbd09f27356cfd14944353e789a9afa4661a70");
+//define("KEY", "none");
