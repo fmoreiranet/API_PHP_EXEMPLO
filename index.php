4bb67d1bd5e05ea5b24a89eeca0299698bbf2797
Fabiano Moreira <fabiano.moreira@atak.com.br>
2022-04-20 21:06:05 -0300

    updates

--- index.php
+++ index.php
@@ -18,7 +18,11 @@ try {
     $auth = null;
 
     //Cabeçalho comum da aplicação
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS");
     header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
     //Validação de rotas
     $method = isset($_SERVER["REQUEST_METHOD"]) ? $_SERVER["REQUEST_METHOD"] : null;
