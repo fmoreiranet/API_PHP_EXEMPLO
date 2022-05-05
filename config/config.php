4bcb027f4880ef0e8c96283e67511650fb2d1e1b
Fabiano Moreira <fabiano.moreira@atak.com.br>
2022-05-05 02:17:02 -0300

    2022-05-04

--- config/config.php
+++ config/config.php
@@ -6,11 +6,17 @@ include("secrets.php");
 // ini_set('display_startup_errors', 1);
 // ini_set("enable_post_data_reading", 1);
 
+//define("DRIVE", "mysql"); // Qual tipo de banco de dados
+//define("PORT", "3306"); // Porta de acesso ao banco de dados
+//define("HOST", "localhost"); // url ou ip do local do banco de dados
+//define("USER", "id18884215_usuariodb"); //Usuario do banco de dados
+//define("DB", "id18884215_loja_exemplo"); //Nome do banco de dados
+
 define("DRIVE", "mysql"); // Qual tipo de banco de dados
-define("HOST", "localhost"); // url ou ip do local do banco de dados
 define("PORT", "3306"); // Porta de acesso ao banco de dados
-define("USER", "id18884215_usuariodb"); //Usuario do banco de dados
-define("DB", "id18884215_loja_exemplo"); //Nome do banco de dados
+define("HOST", "sql210.gratisphphost.info"); // url ou ip do local do banco de dados
+define("USER", "phpgr_15287674"); //Usuario do banco de dados
+define("DB", "phpgr_15287674_loja_exemplo"); //Nome do banco de dados
 
 //define("PASS", ""); // Senha do banco de dados
 //define("KEY", "none");
