e98c456456177d7544eb351ef959dc3d87988c2c
Fabiano Moreira <fabiano.moreira@atak.com.br>
2022-05-05 01:50:04 -0300

    2022-05-04

--- config/config.php
+++ config/config.php
@@ -2,9 +2,9 @@
 include("secrets.php");
 
 // error_reporting(E_ALL);
-// ini_set("display_errors", 0);
-// ini_set('display_startup_errors', 0);
-// ini_set("enable_post_data_reading", 0);
+// ini_set("display_errors", 1);
+// ini_set('display_startup_errors', 1);
+// ini_set("enable_post_data_reading", 1);
 
 define("DRIVE", "mysql"); // Qual tipo de banco de dados
 define("HOST", "localhost"); // url ou ip do local do banco de dados
